6684c03d924fd169f717c04d271f74d8b29876fb
Parth Vaidya <zeusparth99@gmail.com>
2025-03-11 14:03:37 +0530

    Added necessary references to powerup

--- Assets/Scripts/PowerUps/PowerUpService.cs
+++ Assets/Scripts/PowerUps/PowerUpService.cs
@@ -1,3 +1,4 @@
+using CosmicCuration.Utilities;
 using System;
 using UnityEngine;
 
@@ -8,12 +9,14 @@ namespace CosmicCuration.PowerUps
         private PowerUpScriptableObject powerUpScriptableObject;
         private bool isSpawning;
         private float spawnTimer;
+        private PowerUpPool powerUpPool;
 
         public PowerUpService(PowerUpScriptableObject powerUpScriptableObject)
         {
             this.powerUpScriptableObject = powerUpScriptableObject;
             spawnTimer = this.powerUpScriptableObject.spawnRate;
             isSpawning = true;
+            powerUpPool = new PowerUpPool();
         }
 
         public void Update()
@@ -25,6 +28,9 @@ namespace CosmicCuration.PowerUps
                 ResetSpawnTimer();
             }
         }
+
+        public void ReturnPowerUpPool(PowerUpController powerUpToReturn) => powerUpPool.ReturnItem(powerUpToReturn);
+        
 
         private void ResetSpawnTimer() => spawnTimer = powerUpScriptableObject.spawnRate;
 
@@ -50,11 +56,11 @@ namespace CosmicCuration.PowerUps
             switch (typeToFetch)
             {
                 case PowerUpType.Shield:
-                    return new Shield(fetchedData);
+                    return (PowerUpController)powerUpPool.GetPowerUp<Shield>(fetchedData);
                 case PowerUpType.DoubleTurret:
-                    return new DoubleTurret(fetchedData);
+                    return (PowerUpController)powerUpPool.GetPowerUp<DoubleTurret>(fetchedData);
                 case PowerUpType.RapidFire:
-                    return new RapidFire(fetchedData);
+                    return (PowerUpController)powerUpPool.GetPowerUp<RapidFire>(fetchedData);
                 default:
                     throw new Exception($"Failed to Create PowerUpController for: {typeToFetch}");
             }
